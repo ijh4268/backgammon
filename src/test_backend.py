adb4bcd0729c262e9eedc72625d3826aa77f8987
Isaac Henry <isaachenry2022@u.northwestern.edu>
2021-04-09 16:14:09 -0500

    add error checking

--- src/test_backend.py
+++ src/test_backend.py
@@ -5,6 +5,10 @@ from sort_backend import sort
 data = []
 
 for line in sys.stdin:
+  try: 
+    line_data = json.loads(line)
+  except:
+    pass
   data.append(json.loads(line))
 
 result = sort(data)
