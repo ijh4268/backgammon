f0ed0aab64b69387254f1ed2e4ba1f94e395fc7a
Isaac Henry <isaachenry2022@u.northwestern.edu>
2021-04-09 20:49:42 -0500

    fix errors

--- src/sort_frontend.py
+++ src/sort_frontend.py
@@ -1,54 +1,50 @@
 import sys
 import json
+import re
 from sort_backend import sort
-
-# The block comment below is from a tutorial on taking input from stdin
-'''stdin_fileno = sys.stdin
- 
-# Keeps reading from stdin and quits only if the word 'exit' is there
-# This loop, by default does not terminate, since stdin is open
-for line in stdin_fileno:
-    # Remove trailing newline characters using strip()
-    if 'exit' == line.strip():
-        print('Found exit. Terminating the program')
-        exit(0)
-    else:
-        print('Message from sys.stdin: {}'.format(line))''' 
-# End of block comment
 
 data = []
 n = 10 
+valid_nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
 
-"""
-Function to validate incoming JSON data
-"""
-def validate(data):
-    try:
-        json.load(data)
-    except json.JSONDecodeError:
-        return False
-    return True
+NOT_WHITESPACE = re.compile(r'[^\s]')
 
+def decode_stacked(document, pos=0, decoder=json.JSONDecoder()):
+    while True:
+        match = NOT_WHITESPACE.search(document, pos)
+        if not match:
+            return
+        pos = match.start()
+        
+        try:
+            obj, pos = decoder.raw_decode(document, pos)
+        except json.JSONDecodeError:
+            pass
+        yield obj
+
+def chunkify(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+s = sys.stdin.read()
 # Add JSON data to a list of all data
-if validate(sys.stdin): data.append(json.load(sys.stdin))
+for item in decode_stacked(s):
+    data.append(item)
+
+only_dicts = list(filter(lambda x: type(x) == dict, data))
 
 # Filter out any unwanted data
-filtered = filter(lambda x: "content" in x, data)
+filtered = list(filter(lambda x: len(x)==1 and "content" in x and x['content'] in valid_nums, only_dicts))
 
-for item in filtered:
-    if type(item.value) is not int:
-        del(item)
-
-chunks = [filtered[i * n:(i+1) * n] for i in range(len(filtered) + n - 1)]
+# separate data into chuncks of 10
+chunks = list(chunkify(filtered, n))
 
 sorted = []
 for chunk in chunks:
-    sorted.append(sort(chunk))
+    if len(chunk) == 10: sorted.append(sort(chunk))
 
-sorted_json = json.dump(sorted)
+sorted_json = json.dumps(sorted)
 
 sys.stdout.flush()
 sys.stdout.write(sorted_json)
-
-
-
